05b1c616d9f1879fb215a8f129277fea89195c05
Max Tyshkevich <2842778@gmail.com>
2021-11-07 13:28:55 +0300

    feat: task-1 read file

---
+++ 01-read-file/index.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+
+let data = '';
+
+const stream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+
+stream.on('data', (chunk) => {
+  data += chunk;
+});
+
+stream.on('end', () => console.log(data));
