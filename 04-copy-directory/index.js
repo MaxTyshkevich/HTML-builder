a5929f11d0c74f67526e150c85f1f76818e54fca
Max Tyshkevich <2842778@gmail.com>
2021-11-08 22:55:25 +0300

    feat: 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
+const path = require('path');
+const fs = require('fs');
+const fsPromises = fs.promises;
 
+(async function () {
+  const source = path.join(__dirname, 'files');
+  const target = path.join(__dirname, 'files-copy');
+
+  await fsPromises.mkdir(target, {
+    recursive: true,
+  });
+
+  fs.readdir(source, (err, files) => {
+    if (err) console.error(err);
+    else {
+      files.forEach((file) => {
+        fsPromises.copyFile(
+          path.join(source, file),
+          path.join(target, file),
+          fs.constants.COPYFILE_FICLONE
+        );
+      });
+    }
+  });
+})();
